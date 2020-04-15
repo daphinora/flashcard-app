bcc398d3065926e0c34e79c71ee9b500cb93b46e
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-15 14:26:38 -0500

    fixed bcrypt gem, passwords implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)    
+        # binding.pry
         if @user.save
           
         else
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
     has_many :flashcards, through: :user_cards
 
     validates :username, presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: false }
+
+    validates :password, length: { minimum: 10 }
     
     has_secure_password   
 end
