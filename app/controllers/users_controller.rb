d8c34b2d1e74339f67ac1eec11fa452dc8f2a96c
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-15 22:33:52 -0500

    added static_page controller and home path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
-        redirect_to new_user_path
+        
     end
 
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+ 
+  get 'static_page/home'
   get 'sessions/new'
+  get 'static_pages/home'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
