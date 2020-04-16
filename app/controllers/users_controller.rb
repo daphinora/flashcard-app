c5340f8545720767d88d5fb67d0715330a008fe4
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-15 20:33:15 -0500

    login page renders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    # def index
-    #     redirect_to 
-    # end
+    def index
+        redirect_to new_user_path
+    end
 
     def show
         @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # resources :tags
   # resources :user_cards
   resources :flashcards, only: [:index, :show, :new, :edit]
-  resources :users, only: [:show, :new, :edit, :create, :update]
+  resources :users
+  # , only: [:show, :new, :edit, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
